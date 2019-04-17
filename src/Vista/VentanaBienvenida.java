80f84ee1870cc66c8a7e6673b54f9e1029cb91f1
Vero <veronicaolmlop@gmail.com>
2019-04-17 10:27:56 +0200

    He quitado checkbox de sonido

--- src/Vista/VentanaBienvenida.java
+++ src/Vista/VentanaBienvenida.java
@@ -1,53 +1,29 @@
 package Vista;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Container;
-import java.awt.FlowLayout;
+
 import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.SystemColor;
-import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.net.URL;
-
-import javax.swing.BoxLayout;
-import javax.swing.GroupLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JRadioButton;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
-import java.applet.AudioClip;
 
 
 public class VentanaBienvenida extends JFrame {
 
-	private JPanel t;
-	private JPanel b;
-	private JPanel bS;
-	private JLabel titulo;
-	private JButton bComenzar;
-	private JButton bSalida;
-	private JRadioButton JRadioButtonMAction;
-	private URL f;
-	private Image fondo;
-	private AudioClip musica;
+
+	
 	private JPanel contentPane;
 
 			
 	public VentanaBienvenida() {
-		setFont(new Font("Cooper Black", Font.PLAIN, 15));
 		setTitle("El Ahorcado");
 		Image iconoV = new ImageIcon(getClass().getResource("/imagenes/ahorcadobienvenida.png")).getImage();
         setIconImage(iconoV);
@@ -67,12 +43,13 @@ public class VentanaBienvenida extends JFrame {
 		lblTitulo.setBounds(20, -200, 1000,800);
 		contentPane.add(lblTitulo);
 		
-		JCheckBox chckbxSonido = new JCheckBox("Sonido");
-		chckbxSonido.setBackground(SystemColor.activeCaptionBorder);
-		chckbxSonido.setBounds(1050, 700, 72, 25);
-		contentPane.add(chckbxSonido);
+		//JCheckBox chckbxSonido = new JCheckBox("Sonido");
+		//chckbxSonido.setBackground(SystemColor.activeCaptionBorder);
+		//chckbxSonido.setBounds(1050, 700, 72, 25);
+		//contentPane.add(chckbxSonido);
 		
-		JButton btnComenzar = new JButton("");
+		
+		JButton btnComenzar = new JButton();
 		btnComenzar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				Ahorcado1vs2 j2 = new Ahorcado1vs2();
@@ -80,8 +57,7 @@ public class VentanaBienvenida extends JFrame {
 				dispose();
 			}
 		});
-		
-		
+				
 		btnComenzar.setIcon(new ImageIcon(VentanaBienvenida.class.getResource("/imagenes/button_comenzar1.png")));
 		btnComenzar.setBounds(450, 450, 260, 200);
 		btnComenzar.setOpaque(false);
@@ -95,8 +71,7 @@ public class VentanaBienvenida extends JFrame {
 		lblNewLabel.setBounds(0, 0, 1200, 800);
 		contentPane.add(lblNewLabel);
 		
-		
-		
+				
 		setVisible(true);
 	}
 }
